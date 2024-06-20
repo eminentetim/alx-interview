69c982e691cbb24542d33a91f50ac80d77894abf
eminentetim <etimeminent@gmail.com>
2024-06-20 10:18:56 +0100

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,12 +3,9 @@
 Main file for testing
 """
 
-
 def makeChange(coins, total):
     """
-    How many of this type of coin can I get with my money? Okay,
-        I'll take that many. Now, how much money do I have left?
-        And how many coins do I have in my pocket?
+    Determines the fewest number of coins needed to meet a given total.
     """
     if total <= 0:
         return 0
@@ -20,6 +17,8 @@ def makeChange(coins, total):
     # Populate the dp array
     for coin in coins:
         for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+            if dp[x - coin] != float('inf'):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+
     # If dp[total] is still float('inf'), return -1, otherwise return dp[total]
     return dp[total] if dp[total] != float('inf') else -1
